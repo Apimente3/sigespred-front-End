b186dfae8f74b5822d605c554bb39db2ec879ae0
felixmiranda <felixmiranda.net@gmail.com>
2020-11-18 21:56:59 -0500

    uso de componentes

--- src/sigespred/_ddp_partidas/PartidaUpload.js
+++ src/sigespred/_ddp_partidas/PartidaUpload.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Form, FormGroup, Input, Row6 } from "../../components/forms";
+
 import { REGISTRO_PARTIDA_BREADCRUM } from "../../config/breadcrums";
-import WraperLarge from "../m000_common/formContent/Wraper";
+import WraperLarge from "../m000_common/formContent/WraperLarge";
 import * as XLSX from "xlsx";
 import { Link } from "react-router-dom";
 import ComboOptions from "../../components/helpers/ComboOptions";
@@ -10,34 +10,37 @@ import { useAsync } from "react-async-hook";
 import * as helperGets from "../../components/helpers/LoadMaestros";
 import { toastr } from "react-redux-toastr";
 import { initAxiosInterceptors } from "../../config/axios";
-
-
+import {
+  Form,
+  FormGroup,
+  Row6,
+  Row12,
+  RowForm,
+  Select,
+  Input,
+  Options,
+  FormControl,
+  InputInline,
+  FormFooter,
+} from "../../components/forms";
 
 const { $ } = window;
 const Axios = initAxiosInterceptors();
 
-
-
 export const PartidaUpload = () => {
-  var partidasArray = [];
-  //const [partida, setPartida, handleInputChange, reset] = useForm({}, []);
-  const [partida, setPartida] = useState([]);
+  
+  const [partida, setPartida, handleInputChange, reset] = useForm({}, []);
   const [totalcargado, setTotalcargado] = useState(0);
   const [totalnocargado, setTotalnocargado] = useState(0);
-  //const [numCargado, setnumCargado] = useState([])
   const [resultado, setResultado] = useState([]);
-  const resListaProyectos = useAsync(helperGets.helperGetListProyectos, []);
+  const listaProyectos = useAsync(helperGets.helperGetListProyectos, []);
+  const [listaTramos, setListaTramos] = useState(null);
+
   const [itemExcel, setItemExcel] = useState([]);
-  const [dataTramo, setDataTramo] = useState(null);
   const [mostrarData, setMostrarData] = useState(false);
   const [procesando, setProcesando] = useState(false); // no sabemos si hay un usuario autenticado
 
-  function handleInputChange(e) {
-    setPartida({
-      ...partida,
-      [e.target.name]: e.target.value,
-    });
-  }
+
 
   const readExcel = (file) => {
     const promise = new Promise((resolve, reject) => {
@@ -69,33 +72,31 @@ export const PartidaUpload = () => {
 
   async function addPartidasImport(partidas) {
     const { data } = await Axios.post(`/partidaregistral/partidas`, partidas);
-
-    console.log(data);
     return data;
   }
+  const cargarChildrenProyecto = async (idProyecto) => {
+    if (idProyecto) {
+      let dataTramos = await helperGets.helperGetListTramos(idProyecto);
 
-  const handleChangeProyecto = async (e) => {
-    if (e.target.value) {
-      let data = await helperGets.helperGetListTramos(e.target.value);
-      setDataTramo(data);
+      setListaTramos(dataTramos);
     } else {
-      setDataTramo(null);
+      setListaTramos(null);
     }
+  };
+
+
+  const handleFiltrarChildrenProyecto = async (e) => {
+    cargarChildrenProyecto(e.target.value);
   };
 
   const registrar = async (e) => {
     e.preventDefault();
-    setProcesando(true);
+    // setProcesando(true);
     $("#btnguardar").button("loading");
-    // console.log("ENTROOOO");
-    // console.log(itemExcel);
-
-    // console.log(partida)
 
     partida.partidas = itemExcel;
 
     try {
-      //let existe = awaitBuscarPartida()
       let { resultados, countnotupload, countupload } = await addPartidasImport(
         partida
       );
@@ -103,7 +104,7 @@ export const PartidaUpload = () => {
       setTotalnocargado(countnotupload);
       setResultado(resultados);
       setMostrarData(true);
-      setProcesando(false);
+    //   setProcesando(false);
       toastr.success(
         "Proceso Finalizado",
         "Revise el listado para obtener las partidas generadas",
@@ -123,206 +124,144 @@ export const PartidaUpload = () => {
         listbreadcrumb={REGISTRO_PARTIDA_BREADCRUM}
       >
         <Form onSubmit={registrar} autoComplete={false}>
-          <div className="form-group col-lg-12">
-            <fieldset className="mleft-20">
-              <legend>Datos Generales</legend>
-            </fieldset>
-          </div>
-          {/* <Row6 title={""}>
-            <FormGroup
-              label={"Proyecto"}
-              require={true}
-              withControl={6}
-              withLabel={6}
-              ayuda={"Proyecto"}
-            >
-              <Input
-                required={true}
-                // value={usuario.contraseniaAnterior}
-                onChange={handleInputChange}
-                autocomplete={"off"}
-                name={"contraseniaAnterior"}
-                placeholder={"Nueva anterior"}
-                type={"password"}
-              ></Input>
-            </FormGroup>
-          </Row6> */}
-
-          <div className="form-group">
-            <div className="form-group col-lg-6">
-              <fieldset className="mleft-20">
-                {/* <legend>Datos Generales</legend> */}
-                <div className="form-group">
-                  <label className="col-lg-4 control-label">
-                    <span className="obligatorio">* </span>Proyecto
-                  </label>
-                  <div className="col-lg-8">
-                    <select
-                      id="infraestructuraid"
-                      className="form-control input-sm"
-                      name="infraestructuraid"
-                      // value={partida.infraestructuraid}
-                      onChange={(e) => {
-                        handleChangeProyecto(e);
-                        handleInputChange(e);
-                      }}
-                      required
-                      title="El proyecto es requerido"
-                    >
-                      <option value="">--SELECCIONE--</option>
-                      {resListaProyectos.error ? (
-                        "Se produjo un error cargando los proyectos"
-                      ) : resListaProyectos.loading ? (
-                        "Cargando..."
-                      ) : (
-                        <ComboOptions
-                          data={resListaProyectos.result}
-                          valorkey="id"
-                          valornombre="denominacion"
-                        />
-                      )}
-                    </select>
-                  </div>
-                </div>
-
-                <div className="form-group">
-                  <label className="col-lg-4 control-label">Tramo</label>
-                  <div className="col-lg-8">
-                    <select
-                      id="tramoid"
-                      name="tramoid"
-                      className="form-control input-sm"
-                      onChange={handleInputChange}
-                    >
-                      <option value="">--SELECCIONE--</option>
-                      {dataTramo && (
-                        <ComboOptions
-                          data={dataTramo}
-                          valorkey="id"
-                          valornombre="descripcion"
-                        />
-                      )}
-                    </select>
-                  </div>
-                </div>
-
-                <div className="form-group">
-                  <label className="col-lg-4 control-label">Sub Tramo</label>
-                  <div className="col-lg-8">
-                    <input
-                      type="text"
-                      className="form-control input-sm"
-                      id="subtramo"
-                      name="subtramo"
-                      onChange={handleInputChange}
+          <RowForm>
+            <Row12 title={"Datos Generales"}>
+              <Row6>
+                <FormGroup
+                  label={"Proyecto"}
+                  require={true}
+                  ayuda={"Proyecto"}
+                >
+                  <Select
+                    required={true}
+                    value={partida.infraestructuraid || ""}
+                    onChange={(e) => {
+                      handleFiltrarChildrenProyecto(e);
+                      handleInputChange(e);
+                    }}
+                    name={"infraestructuraid"}
+                  >
+                    {listaProyectos.result ? (
+                      <ComboOptions
+                        data={listaProyectos.result}
+                        valorkey="id"
+                        valornombre="denominacion"
+                      />
+                    ) : (
+                      "Cargando..."
+                    )}
+                  </Select>
+                </FormGroup>
+                <FormGroup label={"Tramo"}>
+                  <Select
+                    value={partida.tramoid || ""}
+                    onChange={handleInputChange}
+                    name={"tramoid"}
+                  >
+                    <ComboOptions
+                      data={listaTramos}
+                      valorkey="id"
+                      valornombre="descripcion"
                     />
-                  </div>
-                </div>
-
-                <div className="form-group">
-                  <label className="col-lg-4 control-label">
-                    Seleccione archivo
-                  </label>
-                  <div className="col-lg-8">
+                  </Select>
+                </FormGroup>
+                <FormGroup label={"Subtramo"}>
+                  <Input
+                    value={partida.subtramo || ""}
+                    onChange={handleInputChange}
+                    name={"subtramo"}
+                    placeholder={"Ingrese el subtramo"}
+                    type={"text"}
+                  ></Input>
+                </FormGroup>
+                <FormGroup label={"Plantilla de Carga"} require={true} ayuda={"Seleccione el Excel a importar"}>
+                  <Input
+                    type="file"
+                    onChange={(e) => {
+                      const file = e.target.files[0];
+                      readExcel(file);
+                    }}
+                    required
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Link
+                    to={`/partidas`}
+                    className="btn btn-default btn-sm btn-control"
+                  >
+                    Cancelar
+                  </Link>
+                  <button
+                    id="btnguardar"
+                    type="submit"
+                    className="btn btn-danger btn-sm btn-control"
+                  >
+                    Importar
+                  </button>
+                </FormGroup>
+              </Row6>
+            </Row12>
+            {mostrarData && (
+              <Row12 title={"Información de Carga "}>
+                <Row6>
+                  <FormGroup
+                    label={"Total de Partidas Cargadas"}
+                    withLabel={6}
+                    withControl={6}
+                  >
                     <Input
-                      type="file"
-                      onChange={(e) => {
-                        const file = e.target.files[0];
-                        readExcel(file);
-                      }}
-                    />
-                  </div>
-                </div>
-
-                <div className="form-group">
-                  <label className="col-lg-4 control-label"></label>
-                  <div className="col-lg-8">
-                    <Link
-                      to={`/partidas`}
-                      className="btn btn-default btn-sm btn-control"
-                    >
-                      Cancelar
-                    </Link>
-                    <button
-                      id="btnguardar"
-                      type="submit"
-                      className="btn btn-danger btn-sm btn-control"
-                    >
-                      Importar
-                    </button>
-                  </div>
-                </div>
-                {mostrarData && (
+                      value={totalcargado}
+                      name={"totalcargado"}
+                      type={"text"}
+                      readOnly
+                    ></Input>
+                  </FormGroup>
+                  <FormGroup
+                    label={"Total de Partidas no cargadas"}
+                    withLabel={6}
+                    withControl={6}
+                  >
+                    <Input
+                      value={totalnocargado}
+                      name={"totalnocargado"}
+                      type={"text"}
+                      readOnly
+                    ></Input>
+                  </FormGroup>
                   <div className="form-group">
-                    <fieldset className="mleft-20">
-                      <legend>Información de Carga</legend>
-                      <div class="rTable"></div>
-                      <table className="table table-bordered">
-                        <tbody>
-                          <tr>
-                            <th> Total partidas cargadas:</th>
+                    <table className="tableInside">
+                      <thead>
+                        <tr>
+                          <th scope="col"># Partida</th>
+                          <th scope="col">Mensaje</th>
+                          <th scope="col">Estado</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {resultado.map((d) => (
+                          <tr key={d.id}>
+                            <th>{d.numpartida}</th>
+                            <th>{d.mensaje}</th>
                             <th>
-                              <input
-                                type="text"
-                                readOnly
-                                name="totalcargado"
-                                value={totalcargado}
-                              />
+                              {d.estado == "Cargado" ? (
+                                <span class="label label-success">
+                                  {d.estado}
+                                </span>
+                              ) : (
+                                <span class="label label-danger">
+                                  {d.estado}
+                                </span>
+                              )}
                             </th>
                           </tr>
-                          <tr>
-                            <th> Total partidas no cargadas:</th>
-                            <th>
-                              <input
-                                type="text"
-                                readOnly
-                                name="totalnocargado"
-                                value={totalnocargado}
-                              />
-                            </th>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </fieldset>
+                        ))}
+                      </tbody>
+                    </table>
                   </div>
-                )}
-                {mostrarData && (
-                  <div className="form-group">
-                    <div className="form-group col-lg-12">
-                      <table className="table container">
-                        <thead>
-                          <tr>
-                            <th scope="col"># Partida</th>
-                            <th scope="col">Mensaje</th>
-                            <th scope="col">Estado</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {resultado.map((d) => (
-                            <tr key={d.id}>
-                              <th>{d.numpartida}</th>
-                              <th>{d.mensaje}</th>
-                              <th>
-                                {d.estado == "Cargado" ? (
-                                  <span class="label label-success">
-                                    {d.estado}
-                                  </span>
-                                ) : (
-                                  <span class="label label-danger">
-                                    {d.estado}
-                                  </span>
-                                )}
-                              </th>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                )}
-              </fieldset>
-            </div>
-          </div>
+                </Row6>
+              </Row12>
+            )}
+          </RowForm>
         </Form>
       </WraperLarge>
     </>
